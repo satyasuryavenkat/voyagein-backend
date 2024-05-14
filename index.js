30853fdf02fe2ea991df73740f3494dc44b2c2e4
Nunna Satya Surya Venkat <31370698+satyasuryavenkat@users.noreply.github.com>
2024-05-14 12:48:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import userRoute from './routes/users.js'
 import authRoute from './routes/auth.js'
 import reviewRoute from './routes/reviews.js'
 import bookingRoute from './routes/bookings.js'
-import feedbackRoute from './routes/feedback'
+import feedbackRoute from './routes/feedback.js'
 
 dotenv.config()
 const app = express()
